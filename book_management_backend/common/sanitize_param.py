15c7997fbf5b7c652c2fde3479ceb723464981a5
omid.esmaeili <omidesmaeeli4@gmail.com>
2020-03-31 22:15:12 +0430

    fixed untracked files

--- book_management_backend/common/sanitize_param.py
+++
@@ -1,7 +0,0 @@
-def sanitize_param(param):
-    if param:
-        if type(param) == str:
-            param = param.replace("''", "")
-            param = param.strip()
-        return param
-    return None
--- book_management_backend/config/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    mongodb_connection_string = "mongo"
-    mongodb_db_name = "omides_money_management_db"
-    JWT_SECRET_KEY = "!Haksd5khaOisfhsaof%@412fmoPJODHiy901471-2hioHOUTGD*(!134nfksdhfo092144114Jda;h21oy9(1&"
-    JWT_ACCESS_TOKEN_EXPIRES = 3 * 24 * 60 * 60
-
